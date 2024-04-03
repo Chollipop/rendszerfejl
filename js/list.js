eb5a8f3be7d3989479a00170f4cc705a4cedca94
REET69 <164670248+REET69@users.noreply.github.com>
2024-04-03 17:51:51 +0200

    diskcount+rent history + bug fix

--- js/list.js
+++ js/list.js
@@ -1,6 +1,6 @@
 function Setuser()
 {
-    document.getElementById("user").innerHTML = "user";
+    document.getElementById("user").innerHTML = "user (previous rentals)";
 }
 
 function Return()
@@ -10,10 +10,12 @@ function Return()
 
 async function SetupList()
 {
+    $("#SelectCategory").show();
     let connection = await fetch("php/index.php/allcar");
     let data = await connection.json();
     let listgroup = document.getElementById("ListGroup");
     listgroup.innerHTML = "";
+    $("#SelectCategory").val("-");
     for (var item of data)
     {
         let a = document.createElement("a");
@@ -38,6 +40,7 @@ async function SetupList()
         btn.addEventListener("focus", Reserve);
         btn.innerHTML = "Reserve";
         btn.setAttribute("value", "Reserve");
+        btn.setAttribute("name", "");
 
         small.appendChild(btn);
 
@@ -48,6 +51,11 @@ async function SetupList()
         let p = document.createElement("p");
         p.setAttribute("class", "mb-1");
         p.innerHTML = "Category: " + item["catName"] + " Price:" + item["price"] + " $";
+        if (item["discount"] > 0)
+        {
+            p.innerHTML += `Discount percent: ${item["discount"]}%`;
+            btn.setAttribute("name", `${item["price"] - (item["price"] * (item["discount"]) / 100)}`);
+        }
 
         a.appendChild(p);
 
@@ -105,33 +113,60 @@ async function Reserve()
                 let dateToCheck = new Date(date.format('YYYY-MM-DD'));
                 if (isInDateArray(invalidDates, dateToCheck))
                 {
-
                     return true;
                 }
             }
         },
         function (start, end)
         {
-            document.getElementById(id).disabled = true;
-            const date1 = new Date(start.format('MM/DD/YYYY'));
-            const date2 = new Date(end.format('MM/DD/YYYY'));
-            const diffTime = Math.abs(date2 - date1);
-            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
-
-            alert(`Sikeres foglalás: ${dailyPrice * diffDays} Ft`);
+            var dates = getDates(start, end);
+            let invalidReservation = false;
+            dates.forEach(d =>
+            {
+                if (isInDateArray(invalidDates, d))
+                {
+                    invalidReservation = true;
+                }
+            });
+            if (invalidReservation)
+            {
+                alert("Invalid dates selected");
+            }
+            else
+            {
+                CalcPrice(start, end, id, dailyPrice);
+            }
         },
     );
+}
+
+function CalcPrice(start, end, id, dailyPrice)
+{
+    document.getElementById(id).disabled = true;
+    const date1 = new Date(start.format('MM/DD/YYYY'));
+    const date2 = new Date(end.format('MM/DD/YYYY'));
+    const diffTime = Math.abs(date2 - date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+    let discountedPrice = document.getElementById(id).getAttribute("name");
+
+    if (discountedPrice == "")
+    {
+        alert(`Sikeres foglalás: ${dailyPrice * diffDays} Ft`);
+    }
+    else
+    {
+        alert(`Sikeres foglalás: ${discountedPrice * diffDays} Ft`);
+    }
 }
 
 async function Categories()
 {
     let category = document.getElementById("SelectCategory");
     let connection = await fetch("php/index.php/categories");
-    let data = await connection.json();
+    let cat = await connection.json();
     category.innerHTML = "";
 
-    let cat = [];
-    cat.push("-", "SUV", "Sedan", "Hatchback", "Truck");
+    cat = ["-", ...cat];
 
     for (let j = 0; j < cat.length; j++)
     {
@@ -181,6 +216,7 @@ async function ReworkList()
                 btn.setAttribute("id", `${item["id"]}`);
                 btn.addEventListener("focus", Reserve);
                 btn.setAttribute("value", "Reserve");
+                btn.setAttribute("name", "");
 
                 small.appendChild(btn);
 
@@ -191,6 +227,11 @@ async function ReworkList()
                 let p = document.createElement("p");
                 p.setAttribute("class", "mb-1");
                 p.innerHTML = "Category: " + item["catName"] + " Price:" + item["price"] + " $";
+                if (item["discount"] > 0)
+                {
+                    p.innerHTML += `Discount percent: ${item["discount"]}%`;
+                    btn.setAttribute("name", `${item["price"] - (item["price"] * (item["discount"]) / 100)}`);
+                }
 
                 a.appendChild(p);
 
@@ -200,11 +241,120 @@ async function ReworkList()
     }
 }
 
+async function PreviousRentals()
+{
+    $("#SelectCategory").hide();
+    let username = document.getElementById("user").innerHTML;
+    let send =
+    {
+        "username": username
+    };
+    let connection = await fetch("php/index.php/previousRentals",
+        {
+            method: "POST",
+            body: JSON.stringify(send)
+        });
+    let data = await connection.json();
+
+    let listgroup = document.getElementById("ListGroup");
+    listgroup.innerHTML = "";
+    for (var item of data)
+    {
+        let a = document.createElement("a");
+        a.setAttribute("class", "list-group-item list-group-item-action");
+        a.setAttribute("aria-current", "true");
+
+        let div = document.createElement("div");
+        div.setAttribute("class", "d-flex w-100 justify-content-between");
+
+        let h5 = document.createElement("h5");
+        h5.setAttribute("class", "mb-1");
+        h5.innerHTML = item["brand"] + " " + item["model"];
+
+        div.appendChild(h5);
+
+        let small = document.createElement("small");
+        small.innerHTML = `${item["from_date"]} - ${item["to_date"]}`;
+        const date1 = new Date(item["from_date"]);
+        const date2 = new Date(item["to_date"]);
+        const diffTime = Math.abs(date2 - date1);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+
+        div.appendChild(small);
+
+        a.appendChild(div);
+
+        let p = document.createElement("p");
+        p.setAttribute("class", "mb-1");
+        p.innerHTML = "Category: " + item["catName"] + " Daily price:" + item["daily_price"] + " $";
+        p.innerHTML += ` Total price: ${diffDays * item["daily_price"]} $`;
+
+        a.appendChild(p);
+
+        listgroup.appendChild(a);
+    }
+
+}
+
+async function Discounts()
+{
+    $("#SelectCategory").hide();
+    let connection = await fetch("php/index.php/discounts");
+    let data = await connection.json();
+
+    let listgroup = document.getElementById("ListGroup");
+    listgroup.innerHTML = "";
+    for (var item of data)
+    {
+        let a = document.createElement("a");
+        a.setAttribute("class", "list-group-item list-group-item-action");
+        a.setAttribute("aria-current", "true");
+
+        let div = document.createElement("div");
+        div.setAttribute("class", "d-flex w-100 justify-content-between");
+
+        let h5 = document.createElement("h5");
+        h5.setAttribute("class", "mb-1");
+        h5.innerHTML = item["brand"] + " " + item["model"];
+
+        div.appendChild(h5);
+
+        let small = document.createElement("small");
+
+        let btn = document.createElement("input");
+        btn.setAttribute("class", "btn btn-primary");
+        btn.setAttribute("type", "text");
+        btn.setAttribute("id", `${item["id"]}`);
+        btn.addEventListener("focus", Reserve);
+        btn.innerHTML = "Reserve";
+        btn.setAttribute("value", "Reserve");
+        btn.setAttribute("name", `${item["price"] - (item["price"] * (item["percent"]) / 100)}`);
+
+        small.appendChild(btn);
+
+        div.appendChild(small);
+
+        a.appendChild(div);
+
+        let p = document.createElement("p");
+        p.setAttribute("class", "mb-1");
+        p.innerHTML = "Category: " + item["catName"] + " Price:" + item["price"] + " $";
+        p.innerHTML += ` Discount percentage: ${item["percent"]}% Discounted price: ${item["price"] - (item["price"] * (item["percent"]) / 100)} $`;
+
+        a.appendChild(p);
+
+        listgroup.appendChild(a);
+    }
+}
+
 // Event Listeners
 {
     window.addEventListener("load", Setuser);
     window.addEventListener("load", SetupList);
     window.addEventListener("load", Categories);
+    document.getElementById("list").addEventListener("click", SetupList);
     document.getElementById("logout").addEventListener("click", Return);
     document.getElementById("SelectCategory").addEventListener("change", ReworkList);
+    document.getElementById("user").addEventListener("click", PreviousRentals);
+    document.getElementById("discounts").addEventListener("click", Discounts);
 }
